32e76925e95e4ac4b163442847e1173a42dd3fee
NSaiEPS <sainizameps@gmail.com>
2025-10-27 13:17:39 +0530

    Name & UI changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,14 @@ import "./App.css";
 import "antd/dist/reset.css";
 import { router } from "./Routes/Routes";
 import { RouterProvider } from "react-router";
-import ToastBox from "./components/common/ToastBox/index";
+import ToastBox from "./components/common/ToastBox/index"
 
 function App() {
-  return (
+  
+  return( 
     <>
-      <ToastBox />
-      <RouterProvider router={router} />
-    </>
-  );
+    <ToastBox />
+  <RouterProvider router={router} /></>);
 }
 
 export default App;
