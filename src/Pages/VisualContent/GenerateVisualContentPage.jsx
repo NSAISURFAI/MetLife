c0a56e85257d3f07ad4a2c64c5ea63037dfd650d
NSaiEPS <sainizameps@gmail.com>
2025-11-11 17:26:49 +0530

    image upload option

--- src/Pages/VisualContent/GenerateVisualContentPage.jsx
+++ src/Pages/VisualContent/GenerateVisualContentPage.jsx
@@ -5,15 +5,18 @@ import styles from "./generateVisualContent.module.css";
 import Footer from "../../components/common/mainFooter";
 import copy from "../../assets/copy.svg";
 import reuse from "../../assets/reuse.svg";
-import { NoDataMessage } from "../../components/common/NoDataMessage";
+import upload from "../../assets/upload_icon.svg";
+import FullScreenGradientLoader from "../../components/common/GradientLoader";
 import VisualContentTable from "../../components/common/VisualContentTable/VisualContentTable";
+import { NoDataMessage } from "../../components/common/NoDataMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { getGenerateVisualContentImage } from "../../redux/features/generateVisualSlice";
 import { useParams } from "react-router";
-import FullScreenGradientLoader from "../../components/common/GradientLoader";
+import ImageUploadPopup from "../../components/common/popup/ImageUploadPopup";
 
 const GenerateVisualContentPage = () => {
   const [rows, setRows] = useState([]);
+  const dummyImage = "https://dummyimage.com/50x50/e0e0e0/aaaaaa&text=No+Image";
   const columns = [
     { label: "Scene No.", key: "Scene_No." },
     {
@@ -35,17 +38,32 @@ const GenerateVisualContentPage = () => {
     {
       label: "Visual Image",
       key: "Visual_Image",
-      render: (value, row) => (
-        <img
-          src={value}
-          alt="visual"
+      render: (value, row, setPreviewImage) => (
+        <div
           style={{
-            width: 50,
-            height: 50,
-            objectFit: "cover",
-            borderRadius: 8,
+            width: "50px",
+            height: "50px",
+            padding: "6px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            overflow: "hidden",
+            cursor: "pointer",
           }}
-        />
+          onClick={() => setPreviewImage(value)}
+        >
+          <img
+            src={value}
+            alt="visual"
+            onError={(e) => (e.target.src = dummyImage)}
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              borderRadius: 5,
+            }}
+          />
+        </div>
       ),
     },
   ];
@@ -64,23 +82,28 @@ const GenerateVisualContentPage = () => {
       },
     },
     {
-      icon: <img src={reuse} />,
+      icon: <img src={upload} />,
       onClick: (row) => {
-        // handlePromptRegenerate(row);
+        handleImageUpload(row);
       },
     },
   ];
   const { generateVisualLoader, generateVisualContentData } = useSelector(
     (store) => store.GenerateVisualContent
   );
-  console.log(generateVisualContentData, "check");
+  console.log(generateVisualContentData, "generateVisualContentData");
+  const prompt_batch_id = generateVisualContentData?.prompt_batch_id;
+  const title = generateVisualContentData?.title;
   const dispatch = useDispatch();
   const { id } = useParams();
-  // console.log(id, "check_id");
+  const [popup, setPopup] = useState({
+    type: null,
+    data: null,
+  });
 
   useEffect(() => {
     dispatch(getGenerateVisualContentImage(id));
-  }, [dispatch, id]);
+  }, [id, dispatch]);
 
   useEffect(() => {
     if (generateVisualContentData?.visuals) {
@@ -93,35 +116,49 @@ const GenerateVisualContentPage = () => {
       console.log(item.image_url, "image_url");
       return {
         "Scene_No.": index + 1,
-        Visual_Type: item?.visual_type === "clip" ? "clip" : "image",
-        // Visual_Description:  item?.prompt ?? "-",
-        Visual_Description:
-          item?.visual_type === "clip"
-            ? item?.clip_prompt ?? "-"
-            : item?.prompt ?? "-",
-        Visual_Image: item?.image_url ?? "-",
+        Visual_Type: item?.visual_type,
+        Visual_Description: item?.prompt,
+        Visual_Image: item?.image_uploaded_url ?? item?.image_url ?? "-",
         scene_id: item?.scene_id ?? "",
         prompt_id: item?.prompt_id ?? "",
-        prompt: item?.prompt ?? "",
-        clip_prompt: item?.clip_prompt ?? "",
       };
     });
     setRows(newdata);
+  };
+
+  // Image upload function
+  const handleImageUpload = (data) => {
+    setPopup({
+      type: "upload",
+      data,
+    });
+  };
+
+  const closePopup = () => {
+    setPopup({
+      type: null,
+      data: null,
+    });
+  };
+
+  const handleImageUpdate = (data) => {
+    const updatedRows = rows.map((item) => {
+      if (item.scene_id === data.fieldData.scene_id) {
+        return {
+          ...item,
+          Visual_Image: data.new_image,
+        };
+      }
+      return item;
+    });
+    setRows(updatedRows);
   };
 
   return (
     <>
       <div className={styles.container}>
         <OneFrameHeader />
-        {/* {saveVisualContentLoader && (
-          <FullScreenGradientLoader text="loading..." />
-        )} */}
-        {/* {generateVisualLoader && (
-          <FullScreenGradientLoader
-            text="loading..."
-            loader={generateVisualLoader}
-          />
-        )} */}
+        {generateVisualLoader && <FullScreenGradientLoader text="loading..." />}
         <div className={styles.header}>
           <h2 className={styles.title}>
             {generateVisualContentData?.title || "Visual Content"}
@@ -136,20 +173,30 @@ const GenerateVisualContentPage = () => {
                 rows={rows}
                 actions={actions}
               />
+              {popup.type === "upload" && (
+                <ImageUploadPopup
+                  open={true}
+                  onClose={closePopup}
+                  fieldData={popup.data}
+                  script_id={id}
+                  prompt_batch_id={prompt_batch_id}
+                  title={title}
+                  handleImageUpdate={handleImageUpdate}
+                />
+              )}
+
+              <div className={styles.footerButtons}>
+                <Button variant="contained" className={styles.primaryBtn}>
+                  Audio & Animation Toolkit
+                </Button>
+              </div>
             </>
           ) : (
             <>
               <NoDataMessage filter="false" />
             </>
           )}
-
-          <div className={styles.footerButtons}>
-            <Button variant="contained" className={styles.primaryBtn}>
-              Audio & Animation Toolkit
-            </Button>
-          </div>
         </div>
-
         <Footer />
       </div>
     </>
--- src/redux/features/generateVisualSlice.js
+++ src/redux/features/generateVisualSlice.js
@@ -19,11 +19,32 @@ const GenerateVisualContentSlice = createSlice({
     setGenerateVisualContentData(state, action) {
       state.generateVisualContentData = action.payload;
     },
+    updateGenerateVisual(state, action) {
+      console.log(state.generateVisualContentData, "state_checkl");
+      let actualData = [...state.generateVisualContentData.visuals]?.map(
+        (item) => {
+          console.log(item, action?.payload, "state_checkl");
+
+          let data = { ...item };
+          if (item?.scene_id == action?.payload?.scene_id) {
+            data.image_uploaded_url = action.payload?.new_image;
+          }
+          return data;
+        }
+      );
+
+      let actualVisualGenerateData = { ...state.generateVisualContentData };
+      actualVisualGenerateData.visuals = actualData;
+      state.generateVisualContentData = actualVisualGenerateData;
+    },
   },
 });
 
-export const { setGenerateVisualLoader, setGenerateVisualContentData } =
-  GenerateVisualContentSlice.actions;
+export const {
+  setGenerateVisualLoader,
+  setGenerateVisualContentData,
+  updateGenerateVisual,
+} = GenerateVisualContentSlice.actions;
 
 export default GenerateVisualContentSlice.reducer;
 
@@ -31,15 +52,17 @@ export const postGenerateVisualContentImage = (data) => async (dispatch) => {
   dispatch(setGenerateVisualLoader(true));
   try {
     const response = await api.post("images/generate-images", data);
-    dispatch(setGenerateVisualContentData(response?.data?.visuals));
-    navigateTo(`/generate-visual-page/${response?.data?.script_id}`);
-    toast.success("Visual Image generated successfully");
     console.log(response);
+    if (response?.status) {
+      dispatch(setGenerateVisualContentData(response?.data?.visuals));
+      navigateTo(`/generate-visual-page/${response?.data?.script_id}`);
+      toast.success("Visual Image generated successfully");
+    }
   } catch (error) {
     console.log(error);
     toast.error("Something went wrong!");
   } finally {
-    dispatch(setSaveVisualContentLoader(false));
+    dispatch(setGenerateVisualLoader(false));
   }
 };
 
@@ -48,11 +71,45 @@ export const getGenerateVisualContentImage = (id) => async (dispatch) => {
   dispatch(setGenerateVisualLoader(true));
   try {
     const response = await api.get(`images/${id}`);
-    dispatch(setGenerateVisualContentData(response?.data));
+    if (response?.status) {
+      dispatch(setGenerateVisualContentData(response?.data));
+    }
     console.log(response);
   } catch (error) {
     console.log(error);
   } finally {
-    dispatch(setSaveVisualContentLoader(false));
+    dispatch(setGenerateVisualLoader(false));
+  }
+};
+
+// Image upload functionality
+export const postImageUpload = (data) => async (dispatch) => {
+  dispatch(setGenerateVisualLoader(true));
+  try {
+    const response = await api.post(`images/upload-image`, data);
+    const sceneNumber = parseInt(data.get("scene_number"));
+
+    const newImageUrl =
+      response?.data?.visuals?.[sceneNumber - 1]?.image_uploaded_url;
+
+    const sceneId = response?.data?.visuals?.[sceneNumber - 1]?.scene_id;
+    console.log(response, "check_image_upload_response");
+    // dispatch(
+    //   updateGenerateVisual({
+    //     scene_id: response?.data?.visuals?.[data?.scene_number - 1]?.scene_id,
+    //     new_image:
+    //       response?.data?.visuals?.[data?.scene_number - 1]?.image_uploaded_url,
+    //   })
+    // );
+    dispatch(
+      updateGenerateVisual({
+        scene_id: sceneId,
+        new_image: newImageUrl,
+      })
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    dispatch(setGenerateVisualLoader(false));
   }
 };
