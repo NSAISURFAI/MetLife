02d686857248d3339cd78c11b0a950787ad48fb7
NSaiEPS <sainizameps@gmail.com>
2025-12-09 19:21:25 +0530

    version updated

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { setRouter } from "./utils/navigate";
 function App() {
-  
   setRouter(router);
-  console.log("v1.0.3");
+  console.log("v1.0.4");
   return (
     <>
       {/* <ToastBox /> */}
